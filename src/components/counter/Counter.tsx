ba186ee4e36b546ca45ff904cef9a612df95088f
Anna <blinova.anneta@gmail.com>
2024-05-27 16:39:29 +0200

    update Counter component

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
-import { CounterStateType } from '../../App';
 import s from './Counter.module.css'
 import Button from '../button/Button';
+import { useAppSelector } from '../../hooks/hooks';
+import { incrementCounterAC, resetCounterAC } from '../../reducers/countReducer';
+import { useDispatch } from 'react-redux';
 
 type Props = {
-  updateEditMode: (value: boolean) => void
-  counterState: CounterStateType
-  incrementCounter: () => void
-  resetCounter: () => void
+  updateEditMode: (isEditMode: boolean) => void
 }
 
-export function Counter({counterState, incrementCounter, resetCounter, updateEditMode}: Props) {
+export function Counter({updateEditMode}: Props) {
+  const {count, max, min, minInputValue, maxInputValue} = useAppSelector(state => state.counter)
+  // const count = useAppSelector(state => state.counter.count)
+  // const max = useAppSelector(state => state.counter.max)
+  const dispatch = useDispatch()
+
+
+  const incrementCounterHandler = () => {
+    dispatch(incrementCounterAC(count))
+  }
+
+  const resetCounterHandler = () => {
+    dispatch(resetCounterAC(min))
+  }
 
   const updateEditModeHandler = () => {
     updateEditMode(true)
@@ -18,15 +30,13 @@ export function Counter({counterState, incrementCounter, resetCounter, updateEdi
 
   return (
     <div className={s.counter}>
-      <div className={counterState.counter === counterState.max ? s.valueItem : s.counterItem}>{counterState.counter}</div>
+      <div className={count === max ? s.valueItem : s.counterItem}>{count}</div>
       <div className={s.btnStyle}>
-        <Button className={s.btn} disabled={counterState.counter === counterState.max} onClick={incrementCounter}>inc</Button>
-        <Button className={s.btn} onClick={resetCounter}>reset</Button>
+        <Button className={s.btn} disabled={count === max} onClick={incrementCounterHandler}>inc</Button>
+        <Button className={s.btn} onClick={resetCounterHandler}>reset</Button>
         <Button className={s.btn} onClick={updateEditModeHandler}>settings</Button>
-        {/* <button className={s.btn} disabled={counterState.counter === counterState.max} onClick={incrementCountHandler}>inc</button>
-        <button className={s.btn} onClick={resetCounterHandler}>reset</button>
-        <button className={s.btn} onClick={updateEditModeHandler}>settings</button> */}
       </div>
     </div>
   )
 }
+
