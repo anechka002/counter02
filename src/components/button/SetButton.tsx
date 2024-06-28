7cdac2eaa2bb39dc4db9120b69a36e3ea5a3e5ad
Anna <blinova.anneta@gmail.com>
2024-06-28 12:17:24 +0200

    create SetButton component

---
+++ src/components/button/SetButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import s from '../countValue/CountValue.module.css'
+import Button from './Button'
+import { useAppSelector } from '../../hooks/hooks';
+import { useDispatch } from 'react-redux';
+import { setCounterAC } from '../../reducers/countReducer';
+import { editModeAC } from '../../reducers/modeReducer';
+
+type SetButtonProps = {
+  updateEditMode: (isEditMode: boolean) => void;
+};
+
+export const SetButton = ({updateEditMode}: SetButtonProps) => {
+  const minInputValue = useAppSelector(
+    (state) => state.counter.minInputValue
+  );
+  const maxInputValue = useAppSelector(
+    (state) => state.counter.maxInputValue
+  );
+
+  const dispatch = useDispatch();
+
+  const onClickSettingsHandler = () => {
+    dispatch(setCounterAC(minInputValue))
+    // dispatch(editModeAC(false))
+    updateEditMode(false);
+  };
+
+  return (
+    <div className={s.btnField}>
+        <Button
+          className={s.btn}
+          onClick={onClickSettingsHandler}
+          disabled={minInputValue < 0 || minInputValue >= maxInputValue}
+        >
+          set
+        </Button>
+    </div>
+  )
+}
