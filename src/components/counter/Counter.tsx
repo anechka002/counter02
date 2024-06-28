772efb2e5196a53e51d9272101120949331a42d1
Anna <blinova.anneta@gmail.com>
2024-06-28 08:54:31 +0200

    add ButtonsArea component

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,42 +1,81 @@
-import React, { useState } from 'react';
-import s from './Counter.module.css'
+import React, { useEffect, useState } from 'react';
+import s from './Counter.module.css';
 import Button from '../button/Button';
 import { useAppSelector } from '../../hooks/hooks';
-import { incrementCounterAC, resetCounterAC } from '../../reducers/countReducer';
+import {
+  incrementCounterAC,
+  resetCounterAC,
+} from '../../reducers/countReducer';
 import { useDispatch } from 'react-redux';
 
 type Props = {
-  updateEditMode: (isEditMode: boolean) => void
-}
+  updateEditMode: (isEditMode: boolean) => void;
+};
 
-export function Counter({updateEditMode}: Props) {
-  const {count, max, min, minInputValue, maxInputValue} = useAppSelector(state => state.counter)
-  // const count = useAppSelector(state => state.counter.count)
+export function Counter({ updateEditMode }: Props) {
+  // const {
+  //   count,
+  //   maxInputValue: max,
+  //   minInputValue,
+  // } = useAppSelector((state) => state.counter);
+
   // const max = useAppSelector(state => state.counter.max)
-  const dispatch = useDispatch()
-
-
-  const incrementCounterHandler = () => {
-    dispatch(incrementCounterAC(count))
-  }
-
-  const resetCounterHandler = () => {
-    dispatch(resetCounterAC(min))
-  }
-
-  const updateEditModeHandler = () => {
-    updateEditMode(true)
-  }
 
   return (
     <div className={s.counter}>
-      <div className={count === max ? s.valueItem : s.counterItem}>{count}</div>
-      <div className={s.btnStyle}>
-        <Button className={s.btn} disabled={count === max} onClick={incrementCounterHandler}>inc</Button>
-        <Button className={s.btn} onClick={resetCounterHandler}>reset</Button>
-        <Button className={s.btn} onClick={updateEditModeHandler}>settings</Button>
-      </div>
+      <Screen />
+      <ButtonsArea updateEditMode={updateEditMode} />
     </div>
-  )
+  );
 }
+
+const Screen = () => {
+  const count = useAppSelector((state) => state.counter.count);
+  const isMax = useAppSelector(
+    (state) => state.counter.count === state.counter.maxInputValue
+  );
+  return <div className={isMax ? s.valueItem : s.counterItem}>{count}</div>;
+};
+
+type ButtonsAreaProps = {
+  updateEditMode: (isEditMode: boolean) => void;
+};
+
+const ButtonsArea = ({ updateEditMode }: ButtonsAreaProps) => {
+  const dispatch = useDispatch();
+  const isMax = useAppSelector(
+    (state) => state.counter.count === state.counter.maxInputValue
+  );
+  const isMin = useAppSelector(
+    (state) => state.counter.count === state.counter.minInputValue
+  );
+  const incrementCounterHandler = () => {
+    dispatch(incrementCounterAC());
+  };
+
+  const resetCounterHandler = () => {
+    dispatch(resetCounterAC());
+  };
+
+  const updateEditModeHandler = () => {
+    updateEditMode(true);
+  };
+  return (
+    <div className={s.btnStyle}>
+      <Button
+        className={s.btn}
+        disabled={isMax}
+        onClick={incrementCounterHandler}
+      >
+        inc
+      </Button>
+      <Button className={s.btn} onClick={resetCounterHandler} disabled={isMin}>
+        reset
+      </Button>
+      <Button className={s.btn} onClick={updateEditModeHandler}>
+        settings
+      </Button>
+    </div>
+  );
+};
 
