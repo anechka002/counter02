4128bc9ff2efd7ec0ab0123b1a6ff829f70e2fce
Anna <blinova.anneta@gmail.com>
2024-06-28 10:59:45 +0200

    added the components ScreenValue and SetButton to the CountValue component

--- src/components/countValue/CountValue.tsx
+++ src/components/countValue/CountValue.tsx
@@ -1,14 +1,5 @@
 import React, { ChangeEvent } from 'react';
 import s from './CountValue.module.css';
-import Input from '../input/Input';
-import Button from '../button/Button';
-import {
-  setCounterAC,
-  updateMaxInputValueAC,
-  updateMinInputValueAC,
-} from '../../reducers/countReducer';
-import { useAppSelector } from '../../hooks/hooks';
-import { useDispatch } from 'react-redux';
 import { ScreenValue } from './screenValue/ScreenValue';
 import { SetButton } from '../button/SetButton';
 
@@ -17,66 +8,11 @@ type Props = {
 };
 
 export function CountValue({ updateEditMode }: Props) {
-  const { minInputValue, maxInputValue } = useAppSelector(
-    (state) => state.counter
-  );
   
-  const dispatch = useDispatch();
-
-  const onClickSettingsHandler = () => { 
-    dispatch(setCounterAC(minInputValue))
-    updateEditMode(false);
-  };
-
-  const onChangeMinInputValue = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch(updateMinInputValueAC(+e.currentTarget.value));
-  };
-
-  const onChangeMaxInputValue = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch(updateMaxInputValueAC(+e.currentTarget.value));
-    // updateMinInputValue(+e.currentTarget.value) // можно так
-  };
-
   return (
     <div className={s.counterValue}>
-      {/* <ScreenValue/>
-      <SetButton updateEditMode={updateEditMode}/> */}
-      <div className={s.inputField}>
-        <div>
-          <span>max value:</span>
-          <Input
-            className={
-              minInputValue >= maxInputValue ? s.inputRed : s.inputStyle
-            }
-            type="number"
-            value={maxInputValue}
-            //onChangeValue={value=>updateMaxInputValue(+value)} // так тоже можно
-            onChange={onChangeMaxInputValue}
-          />
-        </div>
-        <div>
-          <span>min value:</span>
-          <Input
-            className={
-              minInputValue < 0 || minInputValue >= maxInputValue
-                ? s.inputRed
-                : s.inputStyle
-            }
-            type={'number'}
-            value={minInputValue}
-            onChange={onChangeMinInputValue}
-          />
-        </div>
-      </div>
-      <div className={s.btnField}>
-        <Button
-          className={s.btn}
-          onClick={onClickSettingsHandler}
-          disabled={minInputValue < 0 || minInputValue >= maxInputValue}
-        >
-          set
-        </Button>
-      </div>
+      <ScreenValue/>
+      <SetButton updateEditMode={updateEditMode}/>     
     </div>
   );
 }
